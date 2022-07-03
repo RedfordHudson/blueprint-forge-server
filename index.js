28f37f2a3204ca5a3cc1574dc1e5f77185519f59
RedfordHudson <redfordfoxhudson@gmail.com>
2022-07-03 13:25:06 -0700

    primordial connection

---
+++ index.js
@@ -0,0 +1,24 @@
+// express
+const express = require('express');
+const app = express();
+app.listen(process.env.PORT || 3001, () => {
+    console.log('Listening on port ${port}!')});
+
+// cors
+const cors = require('cors');
+app.use(cors({origin:'*'}));
+
+// mongoose
+const mongoose = require('mongoose');
+const MONGODB_URI = 'mongodb+srv://RedfordHudson:EvKBVYWr3F688F0s@cluster0.jalzw.mongodb.net/?retryWrites=true&w=majority';
+mongoose.connect(process.env.MONGODB_URI || MONGODB_URI);
+const database = mongoose.connection;
+database.once('open',()=>{console.log('Connected to Database!')});
+
+// API
+const ResourceSchema = require('./schema/schema.resource');
+app.get('/', async (request,response) => {
+    ResourceSchema.find({})
+        .then(resources => response.send(resources))
+        .catch(err => response.status(500).send('Error: '+err));
+});
