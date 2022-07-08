bc9573d8b55e36746de1ea3bf69c27025d973e76
RedfordHudson <redfordfoxhudson@gmail.com>
2022-07-07 18:09:37 -0700

    update by ID

--- routes/route.blueprint.js
+++ routes/route.blueprint.js
@@ -2,10 +2,31 @@ const express = require('express').Router();
 
 const blueprintSchema = require('../schema/schema.blueprint');
 
-express.get('/',(req,res) => {
+express.get('/', async (req,res) => {
     blueprintSchema.find({})
         .then(blueprints => res.json(blueprints))
-        .catch(err => res.status(500).json('Error: '+err));
+        .catch(err => res.status(500).json('ERROR: '+err));
+})
+
+express.get('/:name', async (req,res) => {
+    blueprintSchema.find({
+        'name':req.params.name
+    })
+        .then(blueprint => res.json(blueprint))
+        .catch(err => res.status(500).json('ERROR: '+err));
+})
+
+express.patch('/update/:id', async (req,res) => {
+    blueprintSchema.findById(req.params.id)
+        .then(blueprint => {
+            blueprint.name = req.body.name;
+            blueprint.level = req.body.level;
+            blueprint.nodes = req.body.nodes;
+
+            blueprint.save()
+                .then(() => res.json('Blueprint Updated!'))
+                .catch(err => res.status(500).json('ERROR: '+err))
+        })
 })
 
 module.exports = express;
